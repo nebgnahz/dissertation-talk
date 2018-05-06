25e185501f9d7476e41a087afbfbeb4fce4db896
Ben Zhang <benzh@cs.berkeley.edu>
2018-05-06 08:52:44 -0700

    install full texlive

--- install.sh
+++ install.sh
@@ -22,63 +22,8 @@ tlmgr update --self --all --no-auto-install
 # Keep no backups (not required, simply makes cache bigger)
 tlmgr option -- autobackup 0
 
-# Just including xetex so the cache check above works
-# Needed for any use of xetex even if not testing XeTeX
-tlmgr install xetex
-
 # Needed for TeX Live 2017
-tlmgr install xkeyval
-
-# A kind of minimum set of packages needed
-tlmgr install collection-latex
-
-# Install babel languages
-tlmgr install collection-langeuropean
-
-# Index of packages: http://ctan.mirrors.hoobly.com/systems/texlive/tlnet/archive/
-# Other contrib packages: done as a block to avoid multiple calls to tlmgr
-# pgf includes tikz
-tlmgr install   \
-  exam          \
-  amsmath       \
-  amsthm        \
-  amssymb       \
-  mathtools     \
-  enumerate     \
-  thmtools      \
-  stmaryrd      \
-  xcolor        \
-  pdfpages      \
-  centernot     \
-  pgf           \
-  cancel        \
-  hyperref      \
-  bookmark      \
-  pgfplots      \
-  bm            \
-  listings      \
-  graphicx      \
-  scalerel      \
-  stackengine   \
-  etoolbox      \
-  listofitems   \
-  marvosym      \
-  amsfonts      \
-  opensans      \
-  slantsc       \
-  fancyhdr      \
-  ulem          \
-  algorithms    \
-  algorithmicx  \
-  float         \
-  booktabs      \
-  enumitem      \
-  polynom       \
-  fancyvrb      \
-  makecmds      \
-  multirow      \
-  chngcntr      \
-  imakeidx
+tlmgr install scheme-full
 
 ## Install Fira Fonts
 wget -q https://github.com/mozilla/Fira/archive/4.202.zip
@@ -88,6 +33,6 @@ sudo cp Fira*/otf/Fira* /usr/share/fonts/opentype/Fira/
 sudo fc-cache -fv
 
 ## Install Metropolis theme
-- wget -q https://github.com/matze/mtheme/archive/master.zip
-- unzip -qq master.zip
-- (cd mtheme-master && make sty && make install)
+wget -q https://github.com/matze/mtheme/archive/master.zip
+unzip -qq master.zip
+(cd mtheme-master && make sty && make install)
